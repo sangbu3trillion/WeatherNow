7c3899a4ca430b4f295f12ae7a9b039fac1fbc71
hansung1461 <hansung1461@gmail.com>
2023-06-12 21:32:40 +0900

    2023/06/12 웨더인포 컴포넌트 데이터 패칭(단기 예보) 완료

--- src/components/WeatherPage/WeatherInfo.jsx
+++ src/components/WeatherPage/WeatherInfo.jsx
@@ -1,10 +1,68 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+import {dfs_xy_conv} from '../Utils/XYtranselate';
 
 const WeatherInfo = () => {
+    const [x, setX] = React.useState(null);
+    const [y, setY] = React.useState(null);
+    const [weather, setWeather] = React.useState(null);
+    const [baseTime, setBaseTime] = React.useState(null);
+    let temp = new Date();
+    let hour = temp.getHours();
+    hour = hour - 1;
+    function init() {
+        function success(pos) {
+            const x = pos.coords.latitude;
+            const y = pos.coords.longitude;
+            setBaseTime(hour.toString());
+            setX(x);
+            setY(y);
+        }
+
+        function error(err) {
+            console.log(err);
+        }
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+
+    useEffect(() => {
+        init();
+    }, []);
+
+    useEffect(() => {
+        console.log(baseTime);
+        if (baseTime) {
+            const rs = dfs_xy_conv('toXY', x, y);
+            axios
+                .get(
+                    `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?serviceKey=${
+                        process.env.REACT_APP_API_WEATHER_API
+                    }&numOfRows=60&pageNo=15&base_date=20230612&base_time=${baseTime}00&nx=${Math.floor(
+                        rs.x,
+                    )}&ny=${Math.floor(rs.y)}&dataType=JSON`,
+                )
+                .then(res => {
+                    console.log(res);
+                    setWeather(res.data.response.body.items.item);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }, [baseTime, x, y]);
+
     return (
-        <div>
-            <h1>여기는 현욱님의 페이지입니다 건들면 죽음.</h1>
-        </div>
+        <>
+            <ul>
+                {/* {users.map(user => (
+                    <li key={user.b}>
+                        {user.username} ({user.name})
+                    </li>
+                ))} */}
+            </ul>
+            <br />
+            <button>Try loading again</button>
+        </>
     );
 };
 
