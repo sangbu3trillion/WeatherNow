3ac5e4f8d907e0f29e88727fc65491857d769930
hansung1461 <hansung1461@gmail.com>
2023-06-12 17:42:58 +0900

    현욱님의 첫번째 시범 커밋입니다.

---
+++ src/components/WeatherPage/WeatherInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WeatherInfo = () => {
+    return (
+        <div>
+            <h1>여기는 현욱님의 페이지입니다 건들면 죽음.</h1>
+        </div>
+    );
+};
+
+export default WeatherInfo;
