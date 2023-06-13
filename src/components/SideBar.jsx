f52236d517bb9846dfa833fd6a10ac3e9ca04136
iolagvi28 <iolagvi28@gmail.com>
2023-06-13 09:42:25 +0900

    테일윈드 설정(글씨체 변경) ,사이드 바 컴포넌트 추가,시계 추가

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function SideBar() {
+  return (
+    <div>SideBar</div>
+    <div>
+
+
+
+        
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                jua: ["'Jua', sans-serif"],
+            },
+        },
     },
     plugins: [],
 };
