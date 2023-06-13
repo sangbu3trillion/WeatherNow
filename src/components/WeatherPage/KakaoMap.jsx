ca7c7b7c356bb3e3eb303e422d632cfffa802d99
gitgusgml042933 <gitgusgml042933@gmail.com>
2023-06-13 09:21:09 +0900

    카카오 맵 수정

--- src/components/WeatherPage/KakaoMap.jsx
+++ src/components/WeatherPage/KakaoMap.jsx
@@ -1,39 +1,15 @@
 import React, {useEffect} from 'react';
 import {Map, MapMarker} from 'react-kakao-maps-sdk';
 
-const KakaoMap = () => {
-    return (
-        <Map
-            center={{lat: 33.5563, lng: 126.79581}}
-            style={{width: '200px', height: '200px'}}
-            level={3}
-            className="rounded-lg shadow-xl"
-        >
-            <MapMarker
-                position={{lat: 33.55635, lng: 126.795841}}
-                infoWindowOptions={{disableAutoPan: true}}
-            ></MapMarker>
-        </Map>
-    );
-};
-
 export default KakaoMap;
-// {
-//      <Map
-//                     center={{ lat: x, lng: y }}
-//                     style={{ width: '200px', height: '200px' }}
-//                     level={3}
-//                     className="rounded-lg shadow-xl"
-//                 >
-//                     <MapMarker
-//                         position={{ lat: x, lng: y }}
-//                         infoWindowOptions={{ disableAutoPan: true }}
-//                     ></MapMarker>
-//                 </Map>
-// }
+{
+    <Map center={{lat: x, lng: y}} style={{width: '200px', height: '200px'}} level={3} className="rounded-lg shadow-xl">
+        <MapMarker position={{lat: x, lng: y}} infoWindowOptions={{disableAutoPan: true}}></MapMarker>
+    </Map>;
+}
 
-// <Map center={{lat: 33.5563, lng: 126.79581}} style={{width: '100%', height: '360px'}}>
-//             <MapMarker position={{lat: 33.55635, lng: 126.795841}}>
-//                 <div style={{padding: '42px', backgroundColor: '#fff', color: '#000'}}>Custom Overlay!</div>
-//             </MapMarker>
-//         </Map>
+<Map center={{lat: 33.5563, lng: 126.79581}} style={{width: '100%', height: '360px'}}>
+    <MapMarker position={{lat: 33.55635, lng: 126.795841}}>
+        <div style={{padding: '42px', backgroundColor: '#fff', color: '#000'}}>Custom Overlay!</div>
+    </MapMarker>
+</Map>;
