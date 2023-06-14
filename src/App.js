dc107abc7b3c21ce7909111edc2e2956f8701413
iolagvi28 <iolagvi28@gmail.com>
2023-06-14 11:00:35 +0900

    Auto stash before merge of "Clock_feature" and "origin/Clock_feature"

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< refs/remotes/origin/dev
 import WeatherInfo from './components/WeatherPage/WeatherInfo';
 
 function App() {
@@ -5,6 +6,26 @@ function App() {
         <div>
             <WeatherInfo />
         </div>
+=======
+<<<<<<< Updated upstream
+import KakaoMap from './components/WeatherPage/KakaoMap';
+
+function App() {
+    return (
+        <>
+            <KakaoMap />
+        </>
+=======
+import PlayList from './components/MusicPage/PlayList';
+
+function App() {
+    return (
+        // <div className="flex justify-center items-center h-screen">
+        <div>
+            <PlayList />
+        </div>
+>>>>>>> Stashed changes
+>>>>>>> Auto stash before merge of "Clock_feature" and "origin/Clock_feature"
     );
 }
 
