25a1b93e1a865a8bba3f799ce3f3356300e6977f
iolagvi28 <iolagvi28@gmail.com>
2023-06-14 09:22:47 +0900

    siddBar button 수정, logo.png 수정

--- src/components/Utils/SideBar.jsx
+++ src/components/Utils/SideBar.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const WEATHER = '1';
-const STYLE = '2';
-const MUSIC = '3';
+const WEATHER_i = '1';
+const WEATHER_p = '2';
+const STYLE_i = '3';
+const STYLE_p = '4';
+const MUSIC_i = '5';
+const MUSIC_p = '6';
 
 export default function SideBar() {
     //Handler START
@@ -13,13 +16,16 @@ export default function SideBar() {
     const sideBtnClickHandler = e => {
         console.log('[SideBar] sideBtnClickHandler() CALLED!!');
         switch (e.target.name) {
-            case WEATHER:
+            case WEATHER_i:
+            case WEATHER_p:
                 console.log('[SideBar] WEATHER CLICKED!!');
                 break;
-            case STYLE:
+            case STYLE_i:
+            case STYLE_p:
                 console.log('[SideBar] STYLE CLICKED!!');
                 break;
-            case MUSIC:
+            case MUSIC_i:
+            case MUSIC_p:
                 console.log('[SideBar] MUSIC CLICKED!!');
                 break;
             default:
@@ -42,37 +48,49 @@ export default function SideBar() {
                     id="sidebar"
                     className=" left-36 mt bg-gray-200 w-20 rounded-xl drop-shadow-xl opacity-60 hover:w-72 duration-200 ml-3 "
                 >
-                    <nav role="navigation" className="pl-4 pt-8 mt-4 ">
-                        <div className="mt-4 relative overflow-hidden">
-                            <ul className="nav-list space-y-8" id="nav-list">
-                                <li className="nav-item text-xl active p-2 rounded-l-xl text-white hover:text-gray-400">
+                    <nav role="navigation" className="pl-4 pt-8 mt-4 mb-4 ">
+                        <div className="mt-5 mb-5 relative overflow-hidden">
+                            <ul className="nav-list space-y-8 mb-14" id="nav-list">
+                                <li className="nav-item text-xl active p-2 rounded-l-xl flex gap-5 mb-7 cursor-pointer text-white hover:text-gray-400 ">
                                     <button
                                         onClick={sideBtnClickHandler}
-                                        name={WEATHER}
-                                        className="flex gap-5 mb-7 cursor-pointer"
+                                        name={WEATHER_i}
+                                        className="fas fa-sun fa-2x"
+                                    ></button>
+                                    <button
+                                        onClick={sideBtnClickHandler}
+                                        name={WEATHER_p}
+                                        className="text-2xl font-bold font-gb"
                                     >
-                                        <i className="fas fa-sun fa-2x"></i>
-                                        <p className="text-2xl font-bold font-gb">weather</p>
+                                        weather
                                     </button>
                                 </li>
-                                <li className="nav-item text-xl p-2 rounded-l-xl text-white hover:text-gray-400">
+                                <li className="nav-item text-xl p-2 rounded-l-xl flex gap-5 my-7 cursor-pointer text-white hover:text-gray-400">
                                     <button
                                         onClick={sideBtnClickHandler}
-                                        name={STYLE}
-                                        className="flex gap-5 my-7 cursor-pointer"
+                                        name={STYLE_i}
+                                        className="fas fa-user fa-2x"
+                                    ></button>
+                                    <button
+                                        onClick={sideBtnClickHandler}
+                                        name={STYLE_p}
+                                        className="text-2xl font-bold font-gb"
                                     >
-                                        <i className="fas fa-user fa-2x"></i>
-                                        <p className="text-2xl font-bold font-gb">style</p>
+                                        style
                                     </button>
                                 </li>
-                                <li className="nav-item text-xl p-2 rounded-l-xl  text-white hover:text-gray-400">
+                                <li className="nav-item text-xl p-2 rounded-l-xl flex gap-5 my-7 cursor-pointer text-white hover:text-gray-400">
                                     <button
                                         onClick={sideBtnClickHandler}
-                                        name={MUSIC}
-                                        className="flex gap-5 my-7 mb-20 cursor-pointer"
+                                        name={MUSIC_i}
+                                        className="fas fa-headphones fa-2x"
+                                    ></button>
+                                    <button
+                                        onClick={sideBtnClickHandler}
+                                        name={MUSIC_p}
+                                        className="text-2xl font-bold font-gb"
                                     >
-                                        <i className="fas fa-headphones fa-2x"></i>
-                                        <p className="text-2xl font-bold font-gb">music</p>
+                                        music
                                     </button>
                                 </li>
                             </ul>
