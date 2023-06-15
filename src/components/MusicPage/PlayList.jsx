2de8502d3ebb1fa7da9709d165ed494bda6f6ae8
iolagvi28 <iolagvi28@gmail.com>
2023-06-15 12:34:18 +0900

    feat PlayList.jsx, add PlayList.css

--- src/components/MusicPage/PlayList.jsx
+++ src/components/MusicPage/PlayList.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 import imgs from './logo.png';
+import './PlayList.css';
 
 export default function PlayList() {
     // 음악 차트 데이터를 가져오는 코드 작성
@@ -10,9 +12,89 @@ export default function PlayList() {
         {title: 'Song 4', artist: 'Artist 4', album: 'album 4'},
         {title: 'Song 5', artist: 'Artist 5', album: 'album 5'},
     ];
+
+    const CLIENT_ID = '98268ab8f48f4177a7976f4d2b8a966b';
+    const REDIRECT_URI = 'http://localhost:3000';
+    const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
+    const RESPONSE_TYPE = 'token';
+
+    //axios
+    // axios.get() => .then() '요청성공'과 .catch() '요청실패'
+    const axios = require('axios');
+
+    //login, logout
+    const [token, setToken] = useState('');
+
+    //hover event
+    const [hover, setHover] = useState('');
+
+    //useEffect : token
+    useEffect(() => {
+        const hash = window.location.hash;
+        let token = window.localStorage.getItem('token');
+
+        // getToken()
+
+        if (!token && hash) {
+            token = hash
+                .substring(1)
+                .split('&')
+                .find(elem => elem.startsWith('access_token'))
+                .split('=')[1];
+
+            window.location.hash = '';
+            window.localStorage.setItem('token', token);
+        }
+
+        setToken(token);
+    }, []);
+
+    // logout
+    const logout = () => {
+        setToken('');
+        window.localStorage.removeItem('token');
+    };
+
     return (
         <div className="font-gb w-5/6 my-0 mx-auto">
-            <div className="font-bold text-5xl my-10 grid place-items-left">Today Music</div>
+            <div>
+                <div className="font-bold text-5xl my-10 grid place-items-left">Today Music</div>
+                <div>
+                    <ul>
+                        <li className="my-2">
+                            {' '}
+                            {!token ? (
+                                <a
+                                    className="text-gray-400"
+                                    href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+                                >
+                                    <span className="text-black hover:text-blue-300 text-3xl">Login to Spotify</span>
+                                    &nbsp;
+                                    <span
+                                        className="far fa-circle-question absolute leading-relaxed"
+                                        onMouseEnter={() => setHover(true)}
+                                        onMouseLeave={() => setHover(false)}
+                                    >
+                                        <span className="relative">
+                                            <span>
+                                                {hover && (
+                                                    <span>
+                                                        <p className="font-gb text-2xl">로그인이 필요합니다</p>
+                                                    </span>
+                                                )}
+                                            </span>
+                                        </span>
+                                    </span>
+                                </a>
+                            ) : (
+                                <button onClick={logout} className="hover:text-blue-300 text-3xl">
+                                    Logout
+                                </button>
+                            )}
+                        </li>
+                    </ul>
+                </div>
+            </div>
             <div>
                 <ul className="gap-3 w-auto cursor-pointer grid place-items-center">
                     {/* sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 */}
