f94b43b95cf195f9d0dc861ce2585e2590e61c00
yang <ysotgood@gmail.com>
2023-06-15 14:19:31 +0900

    FIX:  온도계 추가 v1

--- src/components/WeatherPage/DailyList.jsx
+++ src/components/WeatherPage/DailyList.jsx
@@ -79,20 +79,67 @@ export default function DailyList() {
 
     return (
         <div className="flex items-center justify-center h-screen m-auto ">
-            <div className="flex flex-col justify-between w-2/6 p-6 overflow-x-scroll border-2 border-solid shadow-md rounded-xl h-96 scrollbar-track-white scrollbar-thumb-blue-300 scrollbar-thumb-rounded-lg scrollbar-thin ">
+            <div className="flex justify-between w-2/6 p-6 overflow-x-scroll border-2 border-solid shadow-md rounded-xl h-96 scrollbar-track-white scrollbar-thumb-blue-300 scrollbar-thumb-rounded-lg scrollbar-thin ">
                 {isLoaded && (
-                    <div className="flex ">
+                    <div className="flex h=full flex-row w-full justify-center">
                         {weather.map(
                             e =>
                                 e.category === 'TMP' &&
                                 (e.fcstDate === baseDate ||
                                     e.fcstDate[e.fcstDate.length - 1] * 1 === baseDate[baseDate.length - 1] * 1) && (
-                                    <div className="ml-1 mr-5 text-2xl font-bold">{e.fcstValue + '°'}</div>
+                                    <div className="flex flex-col items-center justify-center">
+                                        <div className="ml-1 mr-5 text-2xl font-bold">{e.fcstValue + '°'}</div>
+                                        <div className="flex flex-col-reverse items-center h-full my-4 mr-6 bg-gray-200 rounded-full shadow-lg dark:bg-gray-500">
+                                            <div
+                                                className={`${
+                                                    e.fcstValue < 25
+                                                        ? 'bg-gradient-to-t from-blue-100 to-red-400'
+                                                        : 'bg-gradient-to-t from-red-300 to-red-700'
+                                                }  text-xs font-medium text-blue-100
+                                             text-center p-0.5 leading-none rounded-full flex justify-center items-center 
+                                            
+                                             `}
+                                                style={{height: `${e.fcstValue * 1 + 35}%`}}
+                                            >
+                                                {' '}
+                                                <p> {e.fcstValue + '°'}</p>
+                                            </div>
+                                        </div>
+                                        <div className="mr-5 text-base font-semibold">
+                                            {e.fcstTime[0] + e.fcstTime[1] + ':' + e.fcstTime[2] + e.fcstTime[3]}
+                                        </div>
+                                    </div>
                                 ),
                         )}
                     </div>
                 )}
-
+                {/* {isLoaded && (
+                    <div className="flex justify-between h-full my-5">
+                        {weather.map(
+                            e =>
+                                e.category === 'TMP' &&
+                                (e.fcstDate === baseDate ||
+                                    e.fcstDate[e.fcstDate.length - 1] * 1 === baseDate[baseDate.length - 1] * 1) && (
+                                    <>
+                                        <div className="flex flex-col-reverse w-full h-full mr-6 shadow-lg rou7ded-full bg-1ray-200 dark:bg-gray-700">
+                                            <div
+                                                className={`${
+                                                    e.fcstValue < 25
+                                                        ? 'bg-gradient-to-t from-red-200 to-red-400'
+                                                        : 'bg-gradient-to-t from-red-300 to-red-700'
+                                                }  text-xs font-medium text-blue-100
+                                             text-center p-0.5 leading-none rounded-full flex justify-center items-center `}
+                                                style={{height: `${e.fcstValue * 1 + 35}%`}}
+                                            >
+                                                {' '}
+                                                <p> {e.fcstValue + '°'}</p>
+                                            </div>
+                                        </div>
+                                    </>
+                                ),
+                        )}
+                    </div>
+                )}
                 {isLoaded && (
                     <div className="flex scrollbar scrollbar-track-orange-500 scrollbar-thumb-slate-400">
                         {weather.map(
@@ -106,7 +153,7 @@ export default function DailyList() {
                                 ),
                         )}
                     </div>
-                )}
+                )} */}
             </div>
         </div>
     );
