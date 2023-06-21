ae53978ecbd4ba0cbda0e3a1b51bb08da1f8f34f
yang <ysotgood@gmail.com>
2023-06-21 13:31:50 +0900

    Feat:weatherInfo 컴포넌트 데이터 패칭 방식 변경 ,
    weatherInfo 컴포넌트의 데이터 패칭방식을 내부 aiox 호출 에서 외부 컴포넌트의 리덕스쿼리 훅 전송방식으로 변경

--- src/components/WeatherPage/WeatherMain.jsx
+++ src/components/WeatherPage/WeatherMain.jsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import SideBar from '../Utils/SideBar';
 import KakaoMap from './KakaoMap';
 import WeatherInfo from './WeatherInfo';
 import DailyList from './DailyList';
@@ -16,7 +15,7 @@ const WeatherMain = () => {
 
     let temp = new Date();
     let hour = temp.getHours();
-
+    let weather;
     let year = temp.getFullYear();
     let month = temp.getMonth() + 1 < 10 ? '0' + (temp.getMonth() + 1) : temp.getMonth() + 1;
     let date = temp.getDate();
@@ -56,14 +55,65 @@ const WeatherMain = () => {
     useEffect(() => {
         init();
     }, []);
-    if (weatherData.isLoading) return <div>로딩중...</div>;
-    if (weatherData.isError) return <div>에러발생</div>;
+    if (
+        weatherData.isLoading ||
+        weatherData.isFetching ||
+        baseTime === null ||
+        baseDate === null ||
+        x === null ||
+        y === null
+    )
+        return <div>로딩중...</div>;
+    if (weatherData.error) return <div>에러발생</div>;
     if (!weatherData.data) return <div>데이터 없음</div>;
+    weather = weatherData.data.response.body.items.item;
+    let t;
+    let skyValue;
+    let maxTemp;
+    let minTemp;
+    let reh;
+
+    weather.map(e => {
+        if (e.category === 'TMP' && e.fcstTime === baseTime + '00' && e.fcstDate === baseDate) {
+            t = e.fcstValue;
+            console.log(t, 't map');
+        }
+        if (e.fcstTime === baseTime + '00' && e.fcstDate === baseDate) {
+            if (e.category === 'PTY' && (e.fcstValue === '1' || e.fcstValue === '4' || e.fcstValue === '2'))
+                return (skyValue = 'rain');
+
+            if (e.category === 'PTY' && e.fcstValue === '3') return (skyValue = 'snow');
+
+            if (e.category === 'SKY' && e.fcstValue === '1') {
+                skyValue = 'sun';
+                console.log('sunny');
+            }
+
+            if ((e.category === 'SKY' && e.fcstValue === '3') || e.fcstValue === '4') skyValue = 'cloud';
+        }
+        if (e.fcstDate === baseDate && e.category === 'TMX') {
+            maxTemp = e.fcstValue;
+        }
+        if (e.fcstDate === baseDate && e.category === 'TMN') {
+            minTemp = e.fcstValue;
+        }
+        if (e.fcstTime === baseTime + '00' && e.fcstDate === baseDate && e.category === 'REH') {
+            reh = e.fcstValue;
+        }
+    });
+
     return (
         <div className="flex flex-col justify-center w-full">
             <div className="flex justify-between">
                 <KakaoMap />
-                <WeatherInfo />
+                <WeatherInfo
+                    weather={weather}
+                    skyValue={skyValue}
+                    t={t}
+                    maxTemp={maxTemp}
+                    minTemp={minTemp}
+                    reh={reh}
+                />
                 <WeatherRepAndIdx />
             </div>
             <div className="flex items-center justify-between mt-10 ">
